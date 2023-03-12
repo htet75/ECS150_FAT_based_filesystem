ce9693db1fe4119b468bc64a7c904c26d999c291
htet75 <hmyat@ucdavis.edu>
2023-03-11 19:24:58 -0800

    correction to data structure

--- libfs/fs.c
+++ libfs/fs.c
@@ -8,6 +8,17 @@
 #include "fs.h"
 
 struct superblock{
+	char signature[8]; // Signarure ( must be equal to "ECS150FS")
+	uint16_t total_disk_blocks; // Total amount of blocks of virtual disk
+	uint16_t root_dir_index; // Root directory block index
+	uint16_t data_block_start_index; // Data block start index
+	uint16_t data_block_size; // Amount of data blocks
+	uint8_t FAT_block_size; // Number of blocks for FAT
+	char padding[4079]; // Unused/Padding
+}__attribute__((packed));
+
+/*
+struct superblock{
 	uint8_t signature[8];
 	uint16_t version;
 	uint16_t block_size;
@@ -17,16 +28,76 @@ struct superblock{
 	uint32_t root_start;
 	uint32_t root_blocks;
 };
+*/
 
-struct fat_entry{
-	uint32_t value;
-}__attribute__((packed));
+#define FAT_EOC 0xFFFF // End-of-Chain value
+#define FAT_ENTRIES_PER_BLOCK 2048 // Number of FAT entries per block
 
-struct root_entry{
-	uint8_t filename[FS_FILENAME_LEN];
-	uint32_t start_block;
-	uint32_t num_blocks;
+uint16_t* FAT; // pointer to FAT array
+
+/* Initializing FAT array */
+void init_FAT(uint16_t total_blocks)
+{
+	int num_FAT_blocks = (total_blocks + FAT_ENTRIES_PER_BLOCK -1 ) / FAT_ENTRIES_PER_BLOCK;
+	FAT = calloc(num_FAT_blocks * FAT_ENTRIES_PER_BLOCK, sizeof(uint16_t)); // allocating memory for FAT
+	FAT[0] = FAT_EOC; // Setting first entry to END-of-Chain value
 }
+
+/* Getting the next block in the chain of FAT array */
+uint16_t get_next_block(uint16_t block_index)
+{
+	uint16_t entry = FAT[block_index];
+	if(entry >= FAT_EOC)
+		return FAT_EOC; // End-of-cain
+	return entry;	
+}
+
+/* FAT: set next block in the chain */
+void set_next_block(uint16_t block_index, uint16_t next_block_index)
+{
+	FAT[block_index] = next_block_index;
+}
+
+/* FAT: get the block index of the first block of a file */
+uint16_t get_first_block_index(uint16_t file_block_index) {
+    return file_block_index + 1; // First block index is one past the file block index
+}
+
+/* FAT: get the block index of the last block of a file */
+uint16_t get_last_block_index(uint16_t first_block_index) {
+    uint16_t block_index = first_block_index;
+    while (get_next_block(block_index) != FAT_EOC) {
+        block_index = get_next_block(block_index);
+    }
+    return block_index;
+}
+
+/* FAT: allocating a new block for a file */
+uint16_t allocate_block(uint16_t prev_block_index, uint16_t num_blocks_allocated) {
+    uint16_t block_index = prev_block_index;
+    for (int i = 0; i < num_blocks_allocated; i++) {
+        uint16_t next_block_index = get_next_block(block_index);
+        if (next_block_index == FAT_EOC) {
+            uint16_t new_block_index = block_index + 1;
+            set_next_block(block_index, new_block_index);
+            set_next_block(new_block_index, FAT_EOC);
+            block_index = new_block_index;
+        } else {
+            block_index = next_block_index;
+        }
+    }
+    return block_index;
+}
+
+/* Root Directory data structure */
+struct root_dir_entry {
+	char filename[16];	// Filename ( including NULL character )
+	uint32_t size; // Size of the file (in bytes)
+	uint16_t first_datablock_index; // Index of the first data block
+	char padding[10];
+}__attribute__((packed));
+struct root_dir_entry root_dir[128];
+
 
 /* Global variables to used in the file system */
 static int fs_mounted = 0;	//flag to check if a file system is mounted
